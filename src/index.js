d138ef59833672eb8cee7ab5eece3fac953d5e5e
princesonittn <prince.soni@tothenew.com>
2016-12-28 17:06:05 +0530

    user detail validation

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 /* eslint-disable no-console */
 import React from 'react'
 import {render} from 'react-dom';
-import App from './App'
+// import App from './App'
 import UserDetailApp from './UserDetailApp'
+import UserDetailValidationApp from './UserDetailValidationApp'
 
 let app = document.getElementById('main')
 
 
-render(<UserDetailApp />, app)
+render(<UserDetailValidationApp />, app)
