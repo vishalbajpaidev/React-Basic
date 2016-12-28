9e3a6773a36ca00bdb8f8c3f8c6eab1c8162a8e3
princesonittn <prince.soni@tothenew.com>
2016-12-28 15:21:12 +0530

    temperature task 1

--- src/App.js
+++ src/App.js
@@ -2,16 +2,17 @@
  * Created by ttnd on 27/12/16.
  */
 import React from 'react'
-
+import Water from './Water'
 export default class App extends React.Component {
   constructor() {
-    super()
+    super();
   }
 
   render() {
     return (
       <div>
-        Day 2
+       <h2> Day 2</h2>
+        <Water/>
       </div>
     )
   }
