3388b57069d881f0f03455007129ca8606e58de7
princesonittn <prince.soni@tothenew.com>
2016-12-30 15:30:04 +0530

    scroll to top setup

--- src/App.js
+++ src/App.js
@@ -2,16 +2,24 @@
  * Created by ttnd on 27/12/16.
  */
 import React from 'react'
+import ReactDOM from 'react-dom'
+var FontAwesome = require('react-fontawesome');
+// require('./demo.scss')
 
 export default class App extends React.Component {
   constructor() {
     super()
+
+    this.changeColor = this.changeColor.bind(this)
+  }
+  changeColor(e){
+    window.scrollTo(0, 0)
   }
 
   render() {
     return (
       <div>
-        Working
+        <FontAwesome name='angle-double-up' size='5x' onClick={this.changeColor} ></FontAwesome>
       </div>
     )
   }
