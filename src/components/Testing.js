aff135ff606b27868e4785fa2701858d47403418
princesonittn <prince.soni@tothenew.com>
2017-01-02 17:34:49 +0530

    add routes

---
+++ src/components/Testing.js
@@ -0,0 +1,13 @@
+import React from 'react';
+export default class Testing extends React.Component {
+  constructor() {
+    super();
+  }
+  render() {
+    return (
+      <div>
+        testing.......!!!
+      </div>
+    );
+  }
+}
